13b1abb989d1d820f22954a17ae1d1ea977dc794
Toms Druvaskalns <tomsdruvas@gmail.com>
2021-11-17 13:47:17 +0000

    first lab function further part

--- src/python_functions_practice.py
+++ src/python_functions_practice.py
@@ -31,3 +31,7 @@ def number_to_full_month_name(numb1):
 def number_to_short_month_name(numb1):
     import calendar
     return calendar.month_abbr[numb1]
+
+
+def volume(numb1, numb2, numb3):
+    return numb1 * numb2 * numb3
--- tests/python_functions_test.py
+++ tests/python_functions_test.py
@@ -79,10 +79,10 @@ class TestPythonFunctionPractice(unittest.TestCase):
   #Further
 
   #Given the length of a side of a cube calculate the volume
-  @unittest.skip("delete this line to run the test")
+#   @unittest.skip("delete this line to run the test")
   def test_volume_of_cube(self):
-    #add test code here
-    pass
+        volume_result = volume( 4, 2, 4)
+        self.assertEqual( 32, volume_result )
 
   #Given a String, return the String reversed
   @unittest.skip("delete this line to run the test")
